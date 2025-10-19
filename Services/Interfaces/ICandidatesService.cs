09c78382459674ccfafdfd45099336ce877b3207
zopuu <puzovicveljko02@gmail.com>
2025-10-19 12:00:56 +0200

    feat: candidate servic interface

---
+++ Services/Interfaces/ICandidatesService.cs
@@ -0,0 +1,23 @@
+﻿using HRPlatform.Common.Types;
+using HRPlatform.DTOs;
+
+namespace HRPlatform.Services.Interfaces {
+    public interface ICandidatesService {
+        Task<PagedResult<CandidateDto>> GetAsync(
+            string? name,
+            List<int>? skillIds,
+            string match,
+            int page,
+            int pageSize,
+            string sortBy,
+            string dir
+            );
+        Task<CandidateDto?> GetByIdAsync(int id);
+        Task<CandidateDto> CreateAsync(CandidateCreateRequest request);
+        Task<CandidateDto?> UpdateAsync(int id, CandidateUpdateRequest request);
+        Task DeleteAsync(int id);
+
+        Task<CandidateDto> AssignSkillsAsync(int candidateId, AssignSkillsRequest request);
+        Task<CandidateDto> RemoveSkillAssync(int candidateId, int skillId);
+    }
+}
